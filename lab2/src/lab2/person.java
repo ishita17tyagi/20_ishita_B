c132d3440f2a9119633b9d545fd176026a83896e
Ishita Tyagi <124663097+ishita17tyagi@users.noreply.github.com>
2024-04-15 03:33:24 -0700

    ques 1

---
+++ lab2/src/lab2/person.java
@@ -0,0 +1,59 @@
+package lab2;
+
+public class person {
+	  private String name;
+	  private int age;
+	  public Person(String name,int age){
+		    this.name=name;
+		    this.age=age;
+		  }
+	  public void printInfo(){
+		    System.out.println("Name: "+name+", Age: "+age);
+		  }
+	  public void printInfo(){
+		    System.out.println("Name: "+name+", Age: "+age);
+		  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public class Person {
+
+	  
+
+	  
+
+	  public static void main(String[] args) {
+	    Person person1 = new Person("Alice", 30);
+	    Person person2 = new Person("Bob", 25);
+
+	    person1.printInfo();
+	    person2.printInfo();
+	  }
+	}
+

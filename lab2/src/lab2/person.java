1c916bae0ab1cd3b9875701deabc10dc8d3bc320
Ishita Tyagi <124663097+ishita17tyagi@users.noreply.github.com>
2024-04-15 03:36:16 -0700

    new commit

--- lab2/src/lab2/person.java
+++ lab2/src/lab2/person.java
@@ -1,59 +1,20 @@
 package lab2;
-
 public class person {
 	  private String name;
 	  private int age;
-	  public Person(String name,int age){
+	  public person(String name,int age){
 		    this.name=name;
 		    this.age=age;
 		  }
 	  public void printInfo(){
 		    System.out.println("Name: "+name+", Age: "+age);
 		  }
-	  public void printInfo(){
-		    System.out.println("Name: "+name+", Age: "+age);
+	  public static void main(String[] args){
+		    person person1=new person("Ram", 30);
+		    person person2=new person("Sham", 25);
+		    person1.printInfo();
+		    person2.printInfo();
 		  }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-public class Person {
-
-	  
-
-	  
-
-	  public static void main(String[] args) {
-	    Person person1 = new Person("Alice", 30);
-	    Person person2 = new Person("Bob", 25);
-
-	    person1.printInfo();
-	    person2.printInfo();
-	  }
-	}
 
